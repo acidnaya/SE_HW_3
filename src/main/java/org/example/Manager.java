7d5e04a47c508ad8a3db2bd5220671780e9deec6
acidnaya <acidnaya@gmail.com>
2023-03-19 03:04:10 +0300

    jade first try

---
+++ src/main/java/org/example/Manager.java
@@ -0,0 +1,9 @@
+package org.example;
+import jade.core.Agent;
+
+public class Manager extends Agent {
+    @Override
+    protected void setup() {
+        System.out.println("Hello, I am a manager!");
+    }
+}
